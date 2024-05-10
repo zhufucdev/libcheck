70a6d127372a62dec7e7c90e91719f118d6a010d
zhufucdev <1095291434@qq.com>
2024-05-10 09:33:36 +0800

    :art: deprecated image vector

--- src/main/kotlin/ui/component/Basket.kt
+++ src/main/kotlin/ui/component/Basket.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowRightAlt
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -227,7 +228,7 @@ private fun BorrowDialog(
                     ) {
                         LazyAvatar(borrowingOut.avatarUri, Icons.Default.Book, Modifier.size(80.dp))
                         Icon(
-                            imageVector = Icons.Default.ArrowRightAlt,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowRightAlt,
                             contentDescription = "",
                             modifier = Modifier.size(48.dp).padding(horizontal = PaddingLarge)
                         )
